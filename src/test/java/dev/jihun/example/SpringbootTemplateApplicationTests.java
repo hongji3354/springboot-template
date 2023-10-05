2d495748d45b7dbb8456cbd038bf15cb09fcd316
JiHun Hong <hongji3354@gmail.com>
2023-10-06 00:50:38 +0900

    Init Commit

---
+++ src/test/java/dev/jihun/example/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jihun.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
